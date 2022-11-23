40e41a7004a77afa0090075efaa41756494c2bd8
Jon Toshmatov <info@myitedu.us>
2022-11-23 11:35:41 -0500

    Local env has been set up successfully by Jon Toshmatov

---
+++ resources/views/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <!-- FontAwesome-cdn include -->
+    <link rel="stylesheet" href="/css/all.min.css">
+    <!-- Google fonts include -->
+    <link href="/css/quizo_google_fonts.css" rel="stylesheet">
+    <!-- Bootstrap-css include -->
+    <link rel="stylesheet" href="/quizo/version-10/assets/css/bootstrap.min.css">
+    <!-- Animate-css include -->
+    <link rel="stylesheet" href="/quizo/version-10/assets/css/animate.min.css">
+    <!-- Main-StyleSheet include -->
+    <link rel="stylesheet" href="/quizo/version-10/assets/css/style.css">
+</head>
+<body>
+@yield('content')
+</body>
+</html>
---
+++ resources/views/scripts.blade.php
@@ -0,0 +1,10 @@
+<!-- jQuery-js include -->
+<script src="/quizo/version-10/assets/js/jquery-3.6.0.min.js"></script>
+<!-- Countdown-js include -->
+<script src="/quizo/version-10/assets/js/countdown.js"></script>
+<!-- Bootstrap-js include -->
+<script src="/quizo/version-10/assets/js/bootstrap.min.js"></script>
+<!-- jQuery-validate-js include -->
+<script src="/quizo/version-10/assets/js/jquery.validate.min.js"></script>
+<!-- Custom-js include -->
+<script src="/quizo/version-10/assets/js/script.js"></script>
