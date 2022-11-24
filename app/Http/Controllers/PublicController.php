182ad104c45930398088ba9595474daf09d1f00c
Jon Toshmatov <info@myitedu.us>
2022-11-24 16:54:10 -0500

    Jon created contactus page for Sarvars class

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -9,4 +9,7 @@ class PublicController extends Controller
     public function index(Request $request){
         return view('index');
     }
+    public function contactus(Request $request){
+        return view('contactus');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', [\App\Http\Controllers\PublicController::class, 'index'])->name('home');
+Route::get('/contactus', [\App\Http\Controllers\PublicController::class, 'contactus'])->name('contactus');
