597feed85a74fe7b71dc55ede14dd1dddddb8de4
Jon Toshmatov <info@myitedu.us>
2022-12-03 11:59:24 -0500

    added database file

---
+++ storage/databases/myquiz_dec3.sql
@@ -0,0 +1,148 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.5.5-10.4.21-MariaDB)
+# Database: myquiz
+# Generation Time: 2022-12-03 16:58:51 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table answers
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `answers`;
+
+CREATE TABLE `answers` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) unsigned DEFAULT NULL,
+  `title` varchar(250) DEFAULT NULL,
+  `correct` tinyint(1) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `question_answers` (`question_id`),
+  CONSTRAINT `question_answers` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `answers` WRITE;
+/*!40000 ALTER TABLE `answers` DISABLE KEYS */;
+
+INSERT INTO `answers` (`id`, `question_id`, `title`, `correct`, `created_at`, `updated_at`, `deleted_at`)
+VALUES
+	(1,1,'Chicago',NULL,NULL,NULL,NULL),
+	(2,1,'Los Angeles',NULL,NULL,NULL,NULL),
+	(3,1,'Washington DC',1,NULL,NULL,NULL),
+	(4,1,'Miami',NULL,NULL,NULL,NULL),
+	(5,1,'Philladelphia',NULL,'2022-11-25 10:53:27',NULL,NULL);
+
+/*!40000 ALTER TABLE `answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table categories
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(250) DEFAULT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `image` varchar(250) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+
+INSERT INTO `categories` (`id`, `title`, `description`, `image`, `created_at`, `updated_at`, `deleted_at`)
+VALUES
+	(1,'English',NULL,NULL,'2022-11-25 10:50:24',NULL,NULL),
+	(2,'History',NULL,NULL,'2022-11-25 10:50:24',NULL,NULL),
+	(3,'Geography',NULL,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrP0HtG15L8BtJqKCgWTQAPEb0hCc1IXjFXg&usqp=CAU','2022-11-25 11:13:04',NULL,NULL),
+	(4,'US Government',NULL,NULL,'2022-11-25 10:50:24',NULL,NULL),
+	(5,'Music',NULL,NULL,'2022-11-25 10:50:24',NULL,NULL);
+
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table questions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `questions`;
+
+CREATE TABLE `questions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `title` varchar(250) DEFAULT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+
+INSERT INTO `questions` (`id`, `category_id`, `title`, `description`, `created_at`, `updated_at`, `deleted_at`)
+VALUES
+	(1,3,'What is the capital of the USA?',NULL,'2022-11-25 10:52:15',NULL,NULL);
+
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table user_answers
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user_answers`;
+
+CREATE TABLE `user_answers` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `question_id` int(11) DEFAULT NULL,
+  `answer_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `user_answers` WRITE;
+/*!40000 ALTER TABLE `user_answers` DISABLE KEYS */;
+
+INSERT INTO `user_answers` (`id`, `user_id`, `question_id`, `answer_id`, `created_at`, `updated_at`)
+VALUES
+	(1,99,1,3,'2022-12-03 16:46:56','2022-12-03 16:46:56'),
+	(2,99,2,9,'2022-12-03 16:46:56','2022-12-03 16:46:56'),
+	(3,99,1,3,'2022-12-03 16:58:14','2022-12-03 16:58:14');
+
+/*!40000 ALTER TABLE `user_answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
